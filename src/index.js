99dd3abca9f34d456c30d38d5806db8013f5dfd3
Aseet Dhale <aseetdhale10800@gmail.com>
2024-02-14 07:06:56 +0530

    Setup github.io

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import { App } from "./App";
-import { CANVAS_ID } from "./constants";
-
-// Create an instance of APP
-const app = new App(CANVAS_ID);
-app.engine.runRenderLoop(() => {
-  app.scene.render();
-});
